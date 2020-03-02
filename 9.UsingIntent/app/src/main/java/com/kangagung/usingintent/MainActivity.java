4f59a4607c03699b47d8cfd40b1490bf099da7e6
Agung Tri A <agung.t.atmojo@gmail.com>
2020-03-02 11:20:57 +0700

    Return Result form intent

--- 9.UsingIntent/app/src/main/java/com/kangagung/usingintent/MainActivity.java
+++ 9.UsingIntent/app/src/main/java/com/kangagung/usingintent/MainActivity.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int request_code = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        startActivity(new Intent("com.kangagung.usingintent.SecondActivity"));
+        startActivityForResult(new Intent("com.kangagung.usingintent.SecondActivity"),request_code);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == request_code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(),Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- 9.UsingIntent/app/src/main/java/com/kangagung/usingintent/SecondActivity.java
+++ 9.UsingIntent/app/src/main/java/com/kangagung/usingintent/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.kangagung.usingintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void onClick(View view) {
+        Intent data = new Intent();
+        EditText textName = (EditText) findViewById(R.id.textName);
+        data.setData(Uri.parse(textName.getText().toString()));
+        setResult(RESULT_OK,data);
+        finish();
     }
 }
