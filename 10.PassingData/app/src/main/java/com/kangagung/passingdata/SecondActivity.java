f22c708840409459c9d06af7559975e21f698857
Agung Tri A <agung.t.atmojo@gmail.com>
2020-03-02 12:56:50 +0700

    Add project files Passing data using intent

---
+++ 10.PassingData/app/src/main/java/com/kangagung/passingdata/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.kangagung.passingdata;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toast.makeText(this, getIntent().getStringExtra("str1"),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, Integer.toString(getIntent().getIntExtra("age1",0)),Toast.LENGTH_SHORT).show();
+
+        Bundle bundle = getIntent().getExtras();
+
+        Toast.makeText(this, bundle.getString("str2"),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, Integer.toString(bundle.getInt("Age2")),Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onClick(View view) {
+        Intent i = new Intent();
+
+        i.putExtra("Age3", 45);
+        i.setData(Uri.parse("Something passed back to main activity"));
+        setResult(RESULT_OK,i);
+        finish();
+    }
+}
